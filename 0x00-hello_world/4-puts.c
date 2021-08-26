648f8fc8fb93863855cfe0853265cdca42a14bdd
twilight05 <annenorahusoroh@gmail.com>
2021-08-26 04:19:13 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,6 @@
 #!/bin/bash
 #include<stdio.h>
-int main()
-{
-    char Mystr[] = "Programming is like building a multilingual puzzle";
-
-    puts(Mystr);
+int main(){
+    puts("Programming is like building a multilingual puzzle");
 	return 0;
 }
