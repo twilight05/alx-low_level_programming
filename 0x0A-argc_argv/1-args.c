3eb5c1e17fa97c02eeb08e442e16916d2c1a13c7
twilight05 <annenorahusoroh@gmail.com>
2021-09-22 06:26:35 -0700

    Number of arrays

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: Argument count
+ * @argv: array of arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc - 1);
+(void) argv;
+return (0);
+}
