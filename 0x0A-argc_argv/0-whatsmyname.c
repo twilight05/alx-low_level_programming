525943091ef7f135e697fbb826bb9e56a87fadc2
twilight05 <annenorahusoroh@gmail.com>
2021-09-22 06:18:20 -0700

    My name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: counts the argument
+ * @argv: Array of argument
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+(void) argc;
+printf("%s\n", argv[0]);
+return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This directory contains tasks on argv and argc
